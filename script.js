dc99ff8cdbc7a06a6624673b41153ac59bc7a3bf
JoaoLopesFilho <lopesjoaofilho@gmail.com>
2024-11-03 16:38:33 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const NPC= {
+const infoNpc= {
     nome: document.getElementById("nomeNPC"),
     Idade: document.getElementById("idadeNPC"),
     localização: document.getElementById("localNPC"),
 }
+
+const btn = document.getElementsByClassName("botão");
+
+class NPC{
+    constructor(nomeNPC, idadeNPC, localNPC){
+        this.nomeNPC=nomeNPC;
+        this.idadeNPC=idadeNPC;
+        this.idadeNPC=idadeNPC;
+    }
+}
+
+exibirNPC() {
+    spans[0].innerText = this.nomeNPC
+    spans[1].innerText = this.idadeNPC
+    spans[2].innerText = this.localNPC
+}
+
+btn.addEventListener("click", ()=> {
+    const NPC0 = new NPC(
+        .value,
+    )
+})
